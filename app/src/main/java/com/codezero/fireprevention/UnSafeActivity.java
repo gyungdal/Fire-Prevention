61ad873863b1e39377a26ac997ec5e3aca5e580b
DSM_007 <DSM_007>
2016-04-21 12:41:22 +0900

    nav UI change, add another thing...?

---
+++ app/src/main/java/com/codezero/fireprevention/UnSafeActivity.java
@@ -0,0 +1,9 @@
+package com.codezero.fireprevention;
+
+/**
+ * Created by GyungDal on 2016-04-21.
+ * TODO : Daum 지도 API를 사용하여 마크를 찍음
+ * http://apis.map.daum.net/android/guide/
+ */
+public class UnSafeActivity {
+}
